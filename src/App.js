fc65239b48aef4f66c8860a97bd63c56ab9d8004
kongman551 <eggy551@gmail.com>
2022-04-13 10:54:00 -0400

    creates UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import { Header } from './components/Header';
+import { Balance } from './components/Balance';
+import { IncomeExpense } from './components/IncomeExpense';
+import { Transactions } from './components/Transactions';
+import { AddTransaction } from './components/AddTransaction';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <div className='container'>
+        <Balance />
+        <IncomeExpense />
+        <Transactions />
+        <AddTransaction />
+      </div>
     </div>
   );
 }
---
+++ src/components/Transactions.js
@@ -0,0 +1,11 @@
+export const Transactions = () => {
+  return (
+    <><h3>History</h3>
+      <ul className="list">
+        <li className="minus">
+          Cash <span>-$400</span><button className="delete-btn">x</button>
+        </li>
+      </ul>
+    </>
+  )
+}
