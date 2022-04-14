a7705f659f20b51cec1cf3f961d0c1e3578ef510
kongman551 <eggy551@gmail.com>
2022-04-14 12:47:20 -0400

    adds functionality for user to be able to add income or expenses

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import { Header } from './components/Header';
 import { Balance } from './components/Balance';
 import { IncomeExpense } from './components/IncomeExpense';
-import { Transactions } from './components/Transactions';
+import { TransactionList } from './components/TransactionList';
 import { AddTransaction } from './components/AddTransaction';
+
+import { GlobalProvider } from './context/GlobalState';
+
 import './App.css';
 
 function App() {
   return (
-    <div>
+    <GlobalProvider>
       <Header />
       <div className='container'>
         <Balance />
         <IncomeExpense />
-        <Transactions />
+        <TransactionList />
         <AddTransaction />
       </div>
-    </div>
+    </GlobalProvider>
   );
 }
 
