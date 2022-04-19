0daab9f54b97c63fa8a8b64021469ca9fd46e4fd
kongman551 <eggy551@gmail.com>
2022-04-19 15:25:14 -0400

    integrates REST API backend with frontend

--- client/src/components/TransactionList.js
+++ client/src/components/TransactionList.js
@@ -1,11 +1,16 @@
-import { useContext } from "react"
+import { useContext, useEffect } from "react"
 
 import { Transaction } from "./Transaction"
 
 import { GlobalContext } from "../context/GlobalState"
 
 export const TransactionList = () => {
-  const { transactions } = useContext(GlobalContext)
+  const { transactions, getTransactions } = useContext(GlobalContext);
+
+  useEffect(() => {
+    getTransactions();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <>
