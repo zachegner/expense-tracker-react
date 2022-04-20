84a6374ba0d81bb9a6fee9f9ddd7c62a5949aa0b
kongman551 <eggy551@gmail.com>
2022-04-20 10:29:01 -0400

    fixes connection to backend

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
@@ -12,6 +13,8 @@ const transactions = require('./routes/transactions');
 
 const app = express();
 
+const PORT = process.env.PORT || 5000;
+
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'development') {
@@ -20,6 +23,10 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use('/api/v1/transactions', transactions);
 
-const PORT = process.env.PORT || 5000;
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
